3e2a99bee992e9139cc3ae497c2d04bf1ff7e7fd
Sown13 <sonnh.buh@gmail.com>
2023-05-07 15:17:01 +0700

    Create all Table - done

---
+++ table_init.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE StudentManager;
+Use StudentManager;
+CREATE TABLE Address(
+id INT AUTO_INCREMENT PRIMARY KEY,
+address VARCHAR(255)
+);
+CREATE TABLE Classes(
+id INT AUTO_INCREMENT PRIMARY KEY,
+class_name VARCHAR(100) NOT NULL,
+class_language VARCHAR(100) NOT NULL,
+class_description TEXT
+);
+CREATE TABLE Student(
+id INT AUTO_INCREMENT PRIMARY KEY,
+fullname VARCHAR(255) NOT NULL,
+address_id INT,
+age INT,
+phone VARCHAR(20) UNIQUE NOT NULL,
+class_id INT,
+FOREIGN KEY (address_id) REFERENCES address(id),
+FOREIGN KEY (class_id) REFERENCES classes(id)
+);
+CREATE TABLE Course(
+id INT AUTO_INCREMENT PRIMARY KEY,
+course_name VARCHAR(255) NOT NULL,
+course_description TEXT
+);
+CREATE TABLE Mark(
+id INT AUTO_INCREMENT PRIMARY KEY,
+course_id INT,
+student_id INT,
+mark INT,
+FOREIGN KEY (course_id) REFERENCES course(id),
+FOREIGN KEY (student_id) REFERENCES student(id)
+);
+ALTER TABLE student MODIFY phone INT;
